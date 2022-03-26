63d37edadb16a3d035c012ff7e8f75467891fc82
Patrick Elfert <patrickelfert5@gmail.com>
2022-03-26 11:09:02 +0100

    refactoring, rework meal component

--- src/app/calorie-bar/calorie-bar.component.ts
+++ src/app/calorie-bar/calorie-bar.component.ts
@@ -1,13 +1,18 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {CalorieBarService} from '../calorie-bar.service';
+
 
 @Component({
   selector: 'app-calorie-bar',
   templateUrl: './calorie-bar.component.html',
-  styleUrls: ['./calorie-bar.component.sass']
+  styleUrls: ['./calorie-bar.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalorieBarComponent {
-  constructor(public calorieBarService: CalorieBarService) {
-  }
+export class CalorieBarComponent implements  OnInit{
 
-}
+  constructor(public calorieBarService: CalorieBarService) {}
+
+  ngOnInit(): void {}
+
+
+  }
--- src/app/meal/meal.component.ts
+++ src/app/meal/meal.component.ts
@@ -1,5 +1,6 @@
 import {
   AfterViewChecked,
+  ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   OnInit,
@@ -8,81 +9,150 @@ import {
 import { ModalController } from '@ionic/angular';
 import { IngredientSearchModalComponent } from '../ingredient-search-modal/ingredient-search-modal.component';
 import { MealService } from '../meal.service';
-import { ActivatedRoute } from '@angular/router';
 import { CalorieBarService } from '../calorie-bar.service';
-import { v4 } from 'uuid';
-import { format } from 'date-fns';
-import { take } from 'rxjs/operators';
-import { Meal } from '../interfaces/meal';
+import { Meal, MealPayload } from '../interfaces/meal';
 import { Ingredient } from '../interfaces/ingredient';
+import { ActivatedRoute } from '@angular/router';
+import { map, mergeMap, switchMap, take, tap } from 'rxjs/operators';
+import { doc, docData } from '@angular/fire/firestore';
+import {
+  combineLatest,
+  forkJoin,
+  from,
+  merge,
+  of,
+  Subject,
+  Subscription,
+} from 'rxjs';
 
 @Component({
   selector: 'app-meal',
   templateUrl: './meal.component.html',
   styleUrls: ['./meal.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MealComponent implements OnInit, AfterViewChecked {
   @ViewChild('input', { static: false }) ionInput:
     | { setFocus: () => void }
     | undefined;
-  meal: Meal | undefined;
   name = '';
+
+  selectedMeal$ = this.activatedRoute.paramMap.pipe(
+    map((p) => p.get('id') as string),
+    switchMap((i) =>
+      docData<MealPayload>(
+        doc(this.mealService.mealsCollectionReference, i)
+      ).pipe(map((m) => this.mealService.toMeal(m)))
+    )
+  );
+
+  presentModalAction = new Subject();
+  presentModalAction$ = this.presentModalAction.asObservable().pipe(
+    mergeMap((m) =>
+      from(
+        this.modalController.create({
+          component: IngredientSearchModalComponent,
+        })
+      )
+    ),
+    tap((m) => m.present())
+  );
+
+  onModalClose$ = this.presentModalAction$
+    .pipe(mergeMap((m) => from(m.onWillDismiss())))
+    .pipe(
+      switchMap((modalEvent) =>
+        this.selectedMeal$.pipe(
+          take(1),
+          map((meal) => ({ meal, modalEvent }))
+        )
+      ),
+      map(({ meal, modalEvent }) => {
+        meal.ingredients.push(...(modalEvent.data as Ingredient[]));
+        return meal;
+      }),
+      tap(m => this.calorieBarService.changeCaloriesManualAction.next([]))
+    );
+
+  updateIngredientAction = new Subject<{
+    ingredient: Ingredient;
+    index: number;
+  }>();
+  onUpdateIngredient$ = this.updateIngredientAction.asObservable().pipe(
+    switchMap(({ index, ingredient }) =>
+      this.selectedMeal$.pipe(
+        take(1),
+        map((meal) => ({ index, meal, ingredient }))
+      )
+    ),
+    map(({ index, meal, ingredient }) => {
+      meal.ingredients[index] = ingredient;
+      return meal;
+    })
+  );
+
+  deleteIngredientAction = new Subject<number>();
+  onDeleteIngredient$ = this.deleteIngredientAction.asObservable().pipe(
+    switchMap((index) =>
+      this.selectedMeal$.pipe(
+        take(1),
+        map((meal) => ({ index, meal }))
+      )
+    ),
+    map(({ index, meal }) => {
+      meal.ingredients.splice(index, 1);
+      return meal;
+    })
+  );
+
+  mealNameChangeAction = new Subject<string>();
+  onMealNameChange$ = this.mealNameChangeAction.asObservable().pipe(
+    switchMap((name) =>
+      this.selectedMeal$.pipe(
+        take(1),
+        map((meal) => ({ name, meal }))
+      )
+    ),
+    map(({name, meal}) => new Meal(meal.ingredients, name, meal.id, meal.date))
+  );
+
+  onMealChanged$ = merge(
+    this.onMealNameChange$,
+    this.onDeleteIngredient$,
+    this.onUpdateIngredient$,
+    this.onModalClose$
+  );
+
   private needsFocus = false;
+  private onMealChanged: Subscription | undefined;
+
   constructor(
+    public activatedRoute: ActivatedRoute,
     public modalController: ModalController,
     private calorieBarService: CalorieBarService,
     private changedDetector: ChangeDetectorRef,
-    private mealService: MealService,
-    private activatedRoute: ActivatedRoute
+    private mealService: MealService
   ) {}
 
-  ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(async (params) => {
-      const id = params.get('id');
-      if (id) {
-        this.mealService.subscribeToMeal(id).subscribe((m) => {
-          this.meal = m;
-        });
-      } else {
-        this.meal = new Meal(
-          [],
-          '',
-          v4(),
-          this.mealService.selectedDateChangedAction
-            ? format(
-                (await this.mealService.selectedDateChangedAction
-                  .pipe(take(1))
-                  .toPromise()) ?? new Date(),
-                'MM/dd/yyyy'
-              )
-            : format(new Date(), 'MM/dd/yyyy')
-        );
-      }
-    });
-  }
-
-  public async presentModal(): Promise<void> {
-    const modal = await this.modalController.create({
-      component: IngredientSearchModalComponent,
-    });
-    await modal.present();
-    this.meal?.ingredients.push(
-      ...((await modal.onWillDismiss()).data as Ingredient[])
-    );
-    if (this.meal) {
-      await this.mealService.setMeal(this.meal);
-    }
-  }
+  async ngOnInit(): Promise<void> {}
 
   public async deleteIngredient(index: number): Promise<void> {
-    if (this.meal) {
-      this.meal.ingredients.splice(index, 1);
-      await this.mealService.setMeal(this.meal);
-    }
+    this.deleteIngredientAction.next(index);
   }
 
-  ionViewDidEnter() {
+  public async updateIngredient(ingredient: Ingredient, index: number) {
+    this.updateIngredientAction.next({ ingredient, index });
+  }
+
+  async ionViewDidEnter() {
+    this.onMealChanged = this.onMealChanged$.subscribe((m) => {
+      this.mealService.setMeal(m);
+    });
     this.needsFocus = true;
+  }
+
+  async ionViewDidLeave() {
+    this.onMealChanged?.unsubscribe();
   }
 
   ngAfterViewChecked(): void {
@@ -93,10 +163,11 @@ export class MealComponent implements OnInit, AfterViewChecked {
     }
   }
 
-  public async updateIngredient(ingredient: Ingredient, i: number) {
-    if (this.meal) {
-      this.meal.ingredients[i] = ingredient;
-      await this.mealService.setMeal(this.meal);
-    }
+  public presentModal(): void {
+    this.presentModalAction.next();
+  }
+
+  public setMeal($event: Event): void {
+    this.mealNameChangeAction.next(($event as any).target.value);
   }
 }
