5ba5398f08bef48c93c0ea3153671674f13d4bd2
Patrick Elfert <patrickelfert5@gmail.com>
2023-01-31 20:59:15 +0100

    add getters and setters to external ingredient

--- src/app/interfaces/external-ingredient.ts
+++ src/app/interfaces/external-ingredient.ts
@@ -2,9 +2,22 @@ import { Macros } from '../macros';
 
 export class ExternalIngredient{
 
-  constructor(public name: string, public brand: string, public macros: Macros, public amount: number) {
+  constructor(public name: string, public brand: string, public macros: Macros, public defaultAmount: number) {
+    this.amount = defaultAmount;
   }
 
+  set amount(amount: number) {
+    const { protein, fat, carbs } = this.macros;
+    this.macros = {
+      protein: (protein / this.defaultAmount) * amount,
+      fat: (fat / this.defaultAmount) * amount,
+      carbs: (carbs / this.defaultAmount) * amount,
+    };
+    this.defaultAmount = amount;
+  }
+  get amount(): number {
+    return this.defaultAmount;
+  }
   get calories(): number {
     return (
       this.macros.carbs * 4 + this.macros.fat * 9 + this.macros.protein * 4
