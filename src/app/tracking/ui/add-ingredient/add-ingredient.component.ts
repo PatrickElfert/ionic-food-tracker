62659f49d6ff917403b027c630387ef818f3bf48
Patrick Elfert <patrickelfert5@gmail.com>
2023-01-31 20:58:58 +0100

    recalculate ingredient details on amount change

--- src/app/tracking/ui/add-ingredient/add-ingredient.component.ts
+++ src/app/tracking/ui/add-ingredient/add-ingredient.component.ts
@@ -5,9 +5,23 @@ import {
   Input,
   OnInit,
   Output,
+  ViewChild,
 } from '@angular/core';
 import { Chart, DoughnutController, ArcElement } from 'chart.js';
 import { ExternalIngredient } from '../../../interfaces/external-ingredient';
+import { IonInput } from '@ionic/angular';
+import {
+  BehaviorSubject,
+  combineLatest,
+  merge,
+  Observable,
+  of,
+  ReplaySubject,
+  Subject,
+} from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+import { Ingredient } from '../../../interfaces/ingredient';
+import { Macros } from '../../../macros';
 
 @Component({
   selector: 'app-add-ingredient[ingredient][mealCategories]',
@@ -16,46 +30,77 @@ import { ExternalIngredient } from '../../../interfaces/external-ingredient';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddIngredientComponent implements OnInit {
-  @Input() ingredient!: ExternalIngredient;
+  @Input() set ingredient(value: ExternalIngredient) {
+    this.$ingredientChangedAction.next(value);
+  }
   @Input() mealCategories!: string[];
   @Output() addIngredient = new EventEmitter<{
     externalIngredient: ExternalIngredient;
     selectedMealCategory: string;
   }>();
+
+  $ingredientChangedAction = new ReplaySubject<ExternalIngredient>();
+  $amountChangedAction = new BehaviorSubject<number>(100);
+
+  $ingredient: Observable<ExternalIngredient> = combineLatest([
+    this.$ingredientChangedAction,
+    this.$amountChangedAction,
+  ]).pipe(
+    map(([ingredient, amount]) => {
+      ingredient.amount = Number(amount);
+      return ingredient;
+    })
+  );
+
+  $chart = this.$ingredient.pipe(
+    take(1),
+    tap(() => Chart.register(DoughnutController, ArcElement)),
+    map(({ macros }) => this.getChart(macros))
+  );
+
+  $vm: Observable<{ makeChart: () => Chart; ingredient: ExternalIngredient }> =
+    combineLatest([this.$ingredient, this.$chart]).pipe(
+      map(([ingredient, chart]) => ({ ingredient, makeChart: chart })),
+      tap(({ ingredient }) => console.log(ingredient))
+    );
+
   public selectedMealCategory: string | undefined;
 
-  chart: any;
   proteinColor = 'rgb(70, 85, 195)';
   fatColor = 'rgb(223, 41, 53)';
   carbsColor = 'rgb(255, 193, 7)';
 
   constructor() {}
 
-  ngOnInit() {
-    Chart.register(DoughnutController, ArcElement);
-    const { protein, fat, carbs } = this.ingredient.macros;
-    this.chart = new Chart('MyChart', {
-      type: 'doughnut',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow'],
-        datasets: [
-          {
-            data: [protein, fat, carbs],
-            backgroundColor: [
-              this.proteinColor,
-              this.fatColor,
-              this.carbsColor,
-            ],
-            hoverOffset: 4,
-          },
-        ],
-      },
-    });
+  ngOnInit() {}
+
+  private getChart(macros: Macros) {
+    return () =>
+      new Chart('MyChart', {
+        type: 'doughnut',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow'],
+          datasets: [
+            {
+              data: [macros.protein, macros.fat, macros.carbs],
+              backgroundColor: [
+                this.proteinColor,
+                this.fatColor,
+                this.carbsColor,
+              ],
+              hoverOffset: 4,
+            },
+          ],
+        },
+        options: {
+          cutout: '70%',
+        },
+      });
   }
 
-  onAddIngredient() {
+  onAddIngredient(ingredient: ExternalIngredient) {
     this.addIngredient.emit({
-      externalIngredient: this.ingredient,
+      externalIngredient: ingredient,
       selectedMealCategory: this.selectedMealCategory!,
     });
   }
