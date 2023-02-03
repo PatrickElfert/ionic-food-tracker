445845392d99e24672a31088a2bd3f079e145d44
Patrick Elfert <patrickelfert5@gmail.com>
2023-02-03 08:08:17 +0100

    extract formulars to shared utils

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,14 +1,54 @@
-// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+import {
+  ActivityLevel,
+  CaloricIntakeVariables,
+  Goal,
+} from '../interfaces/caloric-intake-variables';
 
-export function isNotUndefinedOrNull<T>(
+// Type guards
+export const isNotUndefinedOrNull = <T>(
   input: T | undefined | null
-): input is T {
-  return input !== undefined || input !== null;
-}
+): input is T => input !== undefined || input !== null;
 
-export const fieldsNotNullOrUndefined = <T>(
-  input: T
-): input is { [Property in keyof T]: NonNullable<T[Property]> } => Object.values(input).every(
-    (value) => value !== undefined && value !== null
+
+// Formulas
+export const calculateCaloricIntake = (variables: CaloricIntakeVariables) => {
+  const { activityLevel, goal } =
+    variables;
+  const activityFactor = getActivityFactor(activityLevel);
+  const goalFactor = getGoalFactor(goal);
+  const BMR = calculateBmr(variables);
+  const bmrIncludingActivity = BMR * activityFactor;
+  return Math.round((bmrIncludingActivity / 100) * (100 + goalFactor));
+};
+
+const getActivityFactor = (activityLevel: ActivityLevel) => {
+  switch (activityLevel) {
+    case ActivityLevel.sedentary:
+    case ActivityLevel.lightlyActive:
+      return 1.53;
+    case ActivityLevel.active:
+      return 1.76;
+    case ActivityLevel.veryActive:
+      return 2.25;
+  }
+};
+
+const getGoalFactor = (goal: Goal) =>
+  goal === Goal.loss ? -10 : goal === Goal.gain ? 10 : 0;
+
+
+const W_FACTOR = 161;
+const M_FACTOR = 5;
+
+const calculateBmr = ({
+  weightInKg,
+  heightInCm,
+  ageInYears,
+  gender,
+}: CaloricIntakeVariables) => (
+    10 * weightInKg +
+    6.25 * heightInCm -
+    5 * ageInYears +
+    (gender === 'MALE' ? M_FACTOR : W_FACTOR)
   );
 
--- src/app/tracking/data-access/calorie-bar.service.ts
+++ src/app/tracking/data-access/calorie-bar.service.ts
@@ -1,32 +1,15 @@
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
-import { BehaviorSubject, combineLatest } from 'rxjs';
-import { Ingredient } from '../interfaces/ingredient';
+import { map } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
 import { DiaryService } from './diary.service';
 import { UserSettingsService } from '../../shared/data-access/user-settings.service';
-import { CaloricIntakeVariables } from '../../onboarding/interfaces/caloric-intake-variables';
-
-const W_FACTOR = 161;
-const M_FACTOR = 5;
+import { calculateCaloricIntake } from '../../shared/utils/utils';
 
 @Injectable({ providedIn: 'root' })
 export class CalorieBarService {
-  changeCaloriesManualAction = new BehaviorSubject<Ingredient[]>([]);
-  changeCaloriesManualAction$ = this.changeCaloriesManualAction
-    .asObservable()
-    .pipe(
-      map((ingredients) =>
-        ingredients
-          ? ingredients.reduce((acc, m) => {
-              acc += m.calories;
-              return acc;
-            }, 0)
-          : 0
-      )
-    );
 
   caloriesFromMeals$ = this.diaryService.diaryDay$.pipe(
-    map(({meals}) =>
+    map(({ meals }) =>
       meals
         ? meals.reduce((acc, m) => {
             acc += m.calories;
@@ -41,23 +24,23 @@ export class CalorieBarService {
       if (settings.fixedCalories) {
         return settings.fixedCalories;
       } else if (settings.caloricIntakeVariables) {
-        return this.calculateCaloricIntake(settings.caloricIntakeVariables);
+        return calculateCaloricIntake(settings.caloricIntakeVariables);
       }
     })
   );
 
   vm$ = combineLatest([
-    this.changeCaloriesManualAction$,
     this.caloriesFromMeals$,
     this.calorieLimit$,
   ]).pipe(
-    map(([manual, caloriesFromMeals, calorieLimit]) => {
-      const calorieFillWidth = (caloriesFromMeals + manual) / (calorieLimit ?? 0);
-        return {
-          currentCalories: caloriesFromMeals + manual,
-          calorieFillWidth: calorieFillWidth > 1 ? 1 : calorieFillWidth,
-          calorieLimit,
-        };
+    map(([caloriesFromMeals, calorieLimit]) => {
+      const calorieFillWidth =
+        caloriesFromMeals / (calorieLimit ?? 0);
+      return {
+        currentCalories: caloriesFromMeals,
+        calorieFillWidth: calorieFillWidth > 1 ? 1 : calorieFillWidth,
+        calorieLimit,
+      };
     })
   );
 
@@ -65,25 +48,4 @@ export class CalorieBarService {
     public userSettingsService: UserSettingsService,
     public diaryService: DiaryService
   ) {}
-
-  public calculateCaloricIntake(variables: CaloricIntakeVariables) {
-    const { ageInYears, weightInKg, heightInCm, gender, activityLevel, goal } =
-      variables;
-    const activityFactor =
-      activityLevel === 'LIGHTLY ACTIVE'
-        ? 1.53
-        : activityLevel === 'ACTIVE'
-        ? 1.76
-        : activityLevel === 'NOT ACTIVE'
-        ? 1
-        : 2.25;
-    const goalFactor = goal === 'LOSS' ? -10 : goal === 'GAIN' ? 10 : 0;
-    const BMR =
-      10 * weightInKg +
-      6.25 * heightInCm -
-      5 * ageInYears +
-      (gender === 'MALE' ? M_FACTOR : W_FACTOR);
-    const bmrIncludingActivity = BMR * activityFactor;
-    return Math.round((bmrIncludingActivity / 100) * (100 + goalFactor));
-  }
 }
