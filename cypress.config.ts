e816ea3fc7eed47468f20e978c1530962651c950
Patrick Elfert <patrickelfert5@gmail.com>
2023-02-15 17:30:29 +0100

    deactivate cypress video

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   e2e: {
     env: environment,
     baseUrl: environment.baseUrl,
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
